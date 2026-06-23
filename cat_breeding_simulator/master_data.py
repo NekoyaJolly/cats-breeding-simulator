5ade20942e045ea24d7b6231a3c67693fdab0fde
NekoyaJolly <jolly1738810@gmail.com>
2026-06-23 19:03:12 +0900

    feat: 毛色の定義（PHENOTYPE_GENOTYPES）をcat_color_genetic_map.csvから動的にロードするようにリファクタリング

--- cat_breeding_simulator/master_data.py
+++ cat_breeding_simulator/master_data.py
@@ -53,196 +53,192 @@ def _variants(
     return genotypes
 
 
-PHENOTYPE_GENOTYPES: dict[str, dict[str, list[ParentGenotype]]] = {
-    "Black": {
-        "male": _variants(
-            "Black",
-            "male",
-            {},
-            (
-                {"B": ("B", "b")},
-                {"B": ("B", "bl")},
-                {"D": ("D", "d")},
-            ),
-        ),
-        "female": _variants(
-            "Black",
-            "female",
-            {},
-            (
-                {"B": ("B", "b")},
-                {"B": ("B", "bl")},
-                {"D": ("D", "d")},
-            ),
-        ),
-    },
-    "Blue": {
-        "male": _variants(
-            "Blue",
-            "male",
-            {"D": ("d", "d")},
-            (
-                {"B": ("B", "b"), "D": ("d", "d")},
-                {"B": ("B", "bl"), "D": ("d", "d")},
-            ),
-        ),
-        "female": _variants(
-            "Blue",
-            "female",
-            {"D": ("d", "d")},
-            (
-                {"B": ("B", "b"), "D": ("d", "d")},
-                {"B": ("B", "bl"), "D": ("d", "d")},
-            ),
-        ),
-    },
-    "Red": {
-        "male": _variants(
-            "Red",
-            "male",
-            {"O": ("O", "Y"), "A": ("A", "A")},
-            (
-                {"O": ("O", "Y"), "B": ("B", "b"), "A": ("A", "A")},
-                {"O": ("O", "Y"), "D": ("D", "d"), "A": ("A", "A")},
-            ),
-        ),
-        "female": _variants(
-            "Red",
-            "female",
-            {"O": ("O", "O"), "A": ("A", "A")},
-            (
-                {"O": ("O", "O"), "B": ("B", "b"), "A": ("A", "A")},
-                {"O": ("O", "O"), "D": ("D", "d"), "A": ("A", "A")},
-            ),
-        ),
-    },
-    "Cream": {
-        "male": _variants(
-            "Cream",
-            "male",
-            {"O": ("O", "Y"), "D": ("d", "d"), "A": ("A", "A")},
-            (
-                {"O": ("O", "Y"), "D": ("d", "d"), "B": ("B", "b"), "A": ("A", "A")},
-                {"O": ("O", "Y"), "D": ("d", "d"), "B": ("B", "bl"), "A": ("A", "A")},
-            ),
-        ),
-        "female": _variants(
-            "Cream",
-            "female",
-            {"O": ("O", "O"), "D": ("d", "d"), "A": ("A", "A")},
-            (
-                {"O": ("O", "O"), "D": ("d", "d"), "B": ("B", "b"), "A": ("A", "A")},
-                {"O": ("O", "O"), "D": ("d", "d"), "B": ("B", "bl"), "A": ("A", "A")},
-            ),
-        ),
-    },
-    "Mackerel Tabby": {
-        "male": _variants(
-            "Mackerel Tabby",
-            "male",
-            {"A": ("A", "a"), "Mc": ("Mc", "mc")},
-            (
-                {"A": ("A", "a"), "Mc": ("Mc", "mc"), "D": ("D", "d")},
-            ),
-        ),
-        "female": _variants(
-            "Mackerel Tabby",
-            "female",
-            {"A": ("A", "a"), "Mc": ("Mc", "mc")},
-            (
-                {"A": ("A", "a"), "Mc": ("Mc", "mc"), "D": ("D", "d")},
-            ),
-        ),
-    },
-    "Cream Tabby-White": {
-        "male": [
-            _build_genotype(
-                "Cream Tabby-White",
-                "male",
-                O=("O", "Y"),
-                D=("d", "d"),
-                A=("A", "A"),
-                Mc=("Mc", "Mc"),
-                S=("S", "s"),
-            )
-        ],
-        "female": [
-            _build_genotype(
-                "Cream Tabby-White",
-                "female",
-                O=("O", "O"),
-                D=("d", "d"),
-                A=("A", "A"),
-                Mc=("Mc", "Mc"),
-                S=("S", "s"),
-            )
-        ],
-    },
-    "Calico": {
-        "male": [],
-        "female": [
-            _build_genotype("Calico", "female", O=("O", "o"), A=("A", "A"), S=("S", "s")),
-            _build_genotype("Calico", "female", O=("O", "o"), A=("A", "A"), S=("S", "S")),
-        ],
-    },
-    "Dilute Calico": {
-        "male": [],
-        "female": [
-            _build_genotype(
-                "Dilute Calico",
-                "female",
-                O=("O", "o"),
-                D=("d", "d"),
-                A=("A", "A"),
-                S=("S", "s"),
-            ),
-            _build_genotype(
-                "Dilute Calico",
-                "female",
-                O=("O", "o"),
-                D=("d", "d"),
-                A=("A", "A"),
-                S=("S", "S"),
-            ),
-        ],
-    },
-    "Pointed": {
-        "male": [
-            _build_genotype("Pointed", "male", C=("cs", "cs"), A=("a", "a")),
-            _build_genotype("Pointed", "male", C=("cs", "cs"), A=("A", "a"), D=("D", "d")),
-        ],
-        "female": [
-            _build_genotype("Pointed", "female", C=("cs", "cs"), A=("a", "a")),
-            _build_genotype("Pointed", "female", C=("cs", "cs"), A=("A", "a"), D=("D", "d")),
-        ],
-    },
-    "Silver": {
-        "male": [
-            _build_genotype("Silver", "male", A=("A", "A"), I=("I", "i"), Mc=("Mc", "Mc")),
-        ],
-        "female": [
-            _build_genotype("Silver", "female", A=("A", "A"), I=("I", "i"), Mc=("Mc", "Mc")),
-        ],
-    },
-    "Smoke": {
-        "male": [
-            _build_genotype("Smoke", "male", I=("I", "i")),
-        ],
-        "female": [
-            _build_genotype("Smoke", "female", I=("I", "i")),
-        ],
-    },
-    "White": {
-        "male": [
-            _build_genotype("White", "male", W=("W", "w")),
-            _build_genotype("White", "male", W=("W", "W"), D=("D", "d")),
-        ],
-        "female": [
-            _build_genotype("White", "female", W=("W", "w")),
-            _build_genotype("White", "female", W=("W", "W"), D=("D", "d")),
-        ],
-    },
-}
+def _generate_carrier_genotypes(phenotype: str, sex: str, base_loci: dict[str, tuple[str, str]]) -> list[ParentGenotype]:
+    defaults = {
+        "B": ("B", "B"),
+        "D": ("D", "D"),
+        "A": ("a", "a"),
+        "C": ("C", "C"),
+        "W": ("w", "w"),
+        "S": ("s", "s"),
+        "Mc": ("Mc", "Mc"),
+        "Ta": ("ta", "ta"),
+        "Sp": ("sp", "sp"),
+        "I": ("i", "i"),
+        "Wb": ("wb", "wb"),
+    }
+    full_loci = dict(defaults)
+    full_loci.update(base_loci)
+
+    genotypes = []
+    # Base genotype
+    genotypes.append(ParentGenotype(phenotype=phenotype, sex=sex, loci=dict(full_loci)))
+    
+    # Carrier list
+    carriers = []
+    if full_loci.get("B") == ("B", "B"):
+        carriers.append({"B": ("B", "b")})
+        carriers.append({"B": ("B", "bl")})
+    if full_loci.get("D") == ("D", "D"):
+        carriers.append({"D": ("D", "d")})
+    if full_loci.get("A") == ("A", "A"):
+        carriers.append({"A": ("A", "a")})
+    if full_loci.get("C") == ("C", "C"):
+        carriers.append({"C": ("C", "cs")})
+        carriers.append({"C": ("C", "cb")})
+    if full_loci.get("Mc") == ("Mc", "Mc"):
+        carriers.append({"Mc": ("Mc", "mc")})
+    if full_loci.get("Ta") == ("Ta", "Ta"):
+        carriers.append({"Ta": ("Ta", "ta")})
+    if full_loci.get("I") == ("I", "I"):
+        carriers.append({"I": ("I", "i")})
+    if full_loci.get("Wb") == ("Wb", "Wb"):
+        carriers.append({"Wb": ("Wb", "wb")})
+        
+    for carrier in carriers:
+        mod_loci = dict(full_loci)
+        mod_loci.update(carrier)
+        genotypes.append(ParentGenotype(phenotype=phenotype, sex=sex, loci=mod_loci))
+        
+    return genotypes
+
+
+def _load_phenotype_genotypes() -> dict[str, dict[str, list[ParentGenotype]]]:
+    import csv
+    import os
+
+    results = {
+        "Black": {
+            "male": _variants("Black", "male", {}, ({"B": ("B", "b")}, {"B": ("B", "bl")}, {"D": ("D", "d")})),
+            "female": _variants("Black", "female", {}, ({"B": ("B", "b")}, {"B": ("B", "bl")}, {"D": ("D", "d")})),
+        },
+        "Blue": {
+            "male": _variants("Blue", "male", {"D": ("d", "d")}, ({"B": ("B", "b"), "D": ("d", "d")}, {"B": ("B", "bl"), "D": ("d", "d")})),
+            "female": _variants("Blue", "female", {"D": ("d", "d")}, ({"B": ("B", "b"), "D": ("d", "d")}, {"B": ("B", "bl"), "D": ("d", "d")})),
+        },
+        "Red": {
+            "male": _variants("Red", "male", {"O": ("O", "Y"), "A": ("A", "A")}, ({"O": ("O", "Y"), "B": ("B", "b"), "A": ("A", "A")}, {"O": ("O", "Y"), "D": ("D", "d"), "A": ("A", "A")})),
+            "female": _variants("Red", "female", {"O": ("O", "O"), "A": ("A", "A")}, ({"O": ("O", "O"), "B": ("B", "b"), "A": ("A", "A")}, {"O": ("O", "O"), "D": ("D", "d"), "A": ("A", "A")})),
+        },
+        "Cream": {
+            "male": _variants("Cream", "male", {"O": ("O", "Y"), "D": ("d", "d"), "A": ("A", "A")}, ({"O": ("O", "Y"), "D": ("d", "d"), "B": ("B", "b"), "A": ("A", "A")}, {"O": ("O", "Y"), "D": ("d", "d"), "B": ("B", "bl"), "A": ("A", "A")})),
+            "female": _variants("Cream", "female", {"O": ("O", "O"), "D": ("d", "d"), "A": ("A", "A")}, ({"O": ("O", "O"), "D": ("d", "d"), "B": ("B", "b"), "A": ("A", "A")}, {"O": ("O", "O"), "D": ("d", "d"), "B": ("B", "bl"), "A": ("A", "A")})),
+        },
+        "Mackerel Tabby": {
+            "male": _variants("Mackerel Tabby", "male", {"A": ("A", "a"), "Mc": ("Mc", "mc")}, ({"A": ("A", "a"), "Mc": ("Mc", "mc"), "D": ("D", "d")},)),
+            "female": _variants("Mackerel Tabby", "female", {"A": ("A", "a"), "Mc": ("Mc", "mc")}, ({"A": ("A", "a"), "Mc": ("Mc", "mc"), "D": ("D", "d")},)),
+        },
+        "Cream Tabby-White": {
+            "male": [_build_genotype("Cream Tabby-White", "male", O=("O", "Y"), D=("d", "d"), A=("A", "A"), Mc=("Mc", "Mc"), S=("S", "s"))],
+            "female": [_build_genotype("Cream Tabby-White", "female", O=("O", "O"), D=("d", "d"), A=("A", "A"), Mc=("Mc", "Mc"), S=("S", "s"))],
+        },
+        "Calico": {
+            "male": [],
+            "female": [_build_genotype("Calico", "female", O=("O", "o"), A=("A", "A"), S=("S", "s")), _build_genotype("Calico", "female", O=("O", "o"), A=("A", "A"), S=("S", "S"))],
+        },
+        "Dilute Calico": {
+            "male": [],
+            "female": [_build_genotype("Dilute Calico", "female", O=("O", "o"), D=("d", "d"), A=("A", "A"), S=("S", "s")), _build_genotype("Dilute Calico", "female", O=("O", "o"), D=("d", "d"), A=("A", "A"), S=("S", "S"))],
+        },
+        "Pointed": {
+            "male": [_build_genotype("Pointed", "male", C=("cs", "cs"), A=("a", "a")), _build_genotype("Pointed", "male", C=("cs", "cs"), A=("A", "a"), D=("D", "d"))],
+            "female": [_build_genotype("Pointed", "female", C=("cs", "cs"), A=("a", "a")), _build_genotype("Pointed", "female", C=("cs", "cs"), A=("A", "a"), D=("D", "d"))],
+        },
+        "Silver": {
+            "male": [_build_genotype("Silver", "male", A=("A", "A"), I=("I", "i"), Mc=("Mc", "Mc"))],
+            "female": [_build_genotype("Silver", "female", A=("A", "A"), I=("I", "i"), Mc=("Mc", "Mc"))],
+        },
+        "Smoke": {
+            "male": [_build_genotype("Smoke", "male", I=("I", "i"))],
+            "female": [_build_genotype("Smoke", "female", I=("I", "i"))],
+        },
+        "White": {
+            "male": [_build_genotype("White", "male", W=("W", "w")), _build_genotype("White", "male", W=("W", "W"), D=("D", "d"))],
+            "female": [_build_genotype("White", "female", W=("W", "w")), _build_genotype("White", "female", W=("W", "W"), D=("D", "d"))],
+        },
+    }
+
+    filename = "cat_color_genetic_map.csv"
+    filepath = None
+
+    paths_to_try = [
+        filename,
+        os.path.join("docs", "architecture", filename),
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), "docs", "architecture", filename),
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), filename),
+    ]
+    for path in paths_to_try:
+        if os.path.exists(path):
+            filepath = path
+            break
+
+    if not filepath:
+        return results
+
+    try:
+        with open(filepath, mode="r", encoding="utf-8-sig") as f:
+            reader = csv.DictReader(f)
+            locus_cols = [col for col in reader.fieldnames if col.endswith("_Locus")] if reader.fieldnames else []
+            
+            for row in reader:
+                color = row.get("CoatColor")
+                if not color:
+                    continue
+                
+                # Determine B locus from name
+                color_lower = color.lower()
+                if "chocolate" in color_lower or "lilac" in color_lower or "choco" in color_lower:
+                    b_allele = ("b", "b")
+                elif "cinnamon" in color_lower or "fawn" in color_lower:
+                    b_allele = ("bl", "bl")
+                else:
+                    b_allele = ("B", "B")
+                
+                base_loci = {"B": b_allele}
+                
+                for col in locus_cols:
+                    val = row.get(col)
+                    if val and val.strip():
+                        locus_name = col.split("_")[0]
+                        if locus_name == "O":
+                            continue
+                        alleles = tuple(val.strip().split("/"))
+                        if len(alleles) == 2:
+                            base_loci[locus_name] = (alleles[0], alleles[1])
+                
+                # O Locus parsing
+                o_val = row.get("O_Locus", "o/o")
+                o_alleles = tuple(o_val.strip().split("/")) if o_val else ("o", "o")
+                if len(o_alleles) != 2:
+                    o_alleles = ("o", "o")
+                
+                # Build Male Genotypes
+                male_loci = dict(base_loci)
+                if o_alleles == ("O", "o"):
+                    male_variants = []
+                else:
+                    male_o_allele = "O" if "O" in o_alleles else "o"
+                    male_loci["O"] = (male_o_allele, "Y")
+                    male_variants = _generate_carrier_genotypes(color, "male", male_loci)
+                    
+                # Build Female Genotypes
+                female_loci = dict(base_loci)
+                female_loci["O"] = o_alleles
+                female_variants = _generate_carrier_genotypes(color, "female", female_loci)
+                
+                if color not in results:
+                    results[color] = {"male": [], "female": []}
+                
+                results[color]["male"].extend(male_variants)
+                results[color]["female"].extend(female_variants)
+                
+    except Exception:
+        pass
+        
+    return results
+
+
+PHENOTYPE_GENOTYPES = _load_phenotype_genotypes()
 
 def _load_breed_filters() -> dict[str, dict[str, tuple[str, str]]]:
     import csv
